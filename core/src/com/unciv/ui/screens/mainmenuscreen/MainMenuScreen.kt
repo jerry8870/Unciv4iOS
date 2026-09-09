d6b69b213218134e9aea67c9968c66ee74289b05
jerry <ai>
2026-09-09 18:22:22 +0800

    Translate iOS maintenance support across enabled languages

--- core/src/com/unciv/ui/screens/mainmenuscreen/MainMenuScreen.kt
+++ core/src/com/unciv/ui/screens/mainmenuscreen/MainMenuScreen.kt
@@ -211,6 +211,7 @@ class MainMenuScreen: BaseScreen(), RecreateOnResize {
             table.row()
             val support = "Support iOS maintenance".toLabel(fontSize = 18, alignment = Align.center)
             support.setWrap(true)
+            support.touchable = Touchable.enabled
             support.onActivation { game.showVoluntarySupport() }
             table.add(support).colspan(if (singleColumn) 1 else 2)
                 .width(if (singleColumn) 330f else 650f).minHeight(44f).padTop(0f)
